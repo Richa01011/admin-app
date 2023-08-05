1e5e97daa559dea6a9e7bcc0233654092fea7e03
Richa010110 <hotkeys010110@gmail.com>
2023-08-05 04:12:55 +0300

    Fix Orders. Add Reports screen

--- public/js/orders.js
+++ public/js/orders.js
@@ -6,32 +6,34 @@ export const Orders = () => {
 	switchTables(tableRadioButtons, ["#tab-removal-schedule", "#tab-completed-orders", "#tab-unfulfilled-orders"]);
 
 	const tableOrders = document.querySelector("#table-orders");
-	const tablePanes = tableOrders.querySelectorAll(".tab-pane");
-	tablePanes.forEach((tablePane) => {
-		if (tablePane) {
-			const tbody = tablePane.querySelector("tbody");
-			const toggleCheckAllButton = tablePane.querySelector("#toggle-checked-all-btn");
-			const deleteButton = tablePane.querySelector("#delete-order-btn");
-			const editButton = tablePane.querySelector("#edit-order-btn");
-			const printButton = tablePane.querySelector("#print-order-btn");
-			const excelButton = tablePane.querySelector("#excel-order-btn");
-			tableCheckboxes(tbody, [deleteButton, editButton, printButton, excelButton], toggleCheckAllButton);
+	if (tableOrders) {
+		const tablePanes = tableOrders.querySelectorAll(".tab-pane");
+		tablePanes.forEach((tablePane) => {
+			if (tablePane) {
+				const tbody = tablePane.querySelector("tbody");
+				const toggleCheckAllButton = tablePane.querySelector("#toggle-checked-all-btn");
+				const deleteButton = tablePane.querySelector("#delete-order-btn");
+				const editButton = tablePane.querySelector("#edit-order-btn");
+				const printButton = tablePane.querySelector("#print-order-btn");
+				const excelButton = tablePane.querySelector("#excel-order-btn");
+				tableCheckboxes(tbody, [deleteButton, editButton, printButton, excelButton], toggleCheckAllButton);
 
-			if (editButton) {
-				editButton.addEventListener("click", () => {
-					fetch("./render-elements/modals/orders/edit-order-modal.html")
-						.then((response) => response.text())
-						.then((text) => {
-							document.body.insertAdjacentHTML("beforeend", text);
-							const editOrderModal = new bootstrap.Modal(document.querySelector("#edit-order-modal"));
+				if (editButton) {
+					editButton.addEventListener("click", () => {
+						fetch("./render-elements/modals/orders/edit-order-modal.html")
+							.then((response) => response.text())
+							.then((text) => {
+								document.body.insertAdjacentHTML("beforeend", text);
+								const editOrderModal = new bootstrap.Modal(document.querySelector("#edit-order-modal"));
 
-							editOrderModal.show();
-							editOrderModal._element.addEventListener("hidden.bs.modal", () => {
-								editOrderModal._element.remove();
+								editOrderModal.show();
+								editOrderModal._element.addEventListener("hidden.bs.modal", () => {
+									editOrderModal._element.remove();
+								});
 							});
-						});
-				});
+					});
+				}
 			}
-		}
-	});
+		});
+	}
 };
